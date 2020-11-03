b5817745667db8ff5c8b6489701a7cd8467ef3e1
Clementine <clementine.fouque@gmail.com>
2020-11-03 15:56:00 +0100

    devise connectée

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,42 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 require 'faker'
+require 'time'
 
-5.times do
-
-  User.create!(
-    email: ("filou78@yopmail.com"),
-    encrypted_password: "azerty",
-    description: Faker::GreekPhilosophers.quote,
-    first_name: Faker::GreekPhilosophers.name,
-    last_name: Faker::Hipster.word
-  )
-
+#create random users
+10.times do
+  User.create!(email: Faker::Name.first_name+Faker::Name.last_name+"@yopmail.com",
+  encrypted_password: ["yellow", "coucou", "marseille", "password", "supersuper"].sample,
+  description: Faker::TvShows::Community.quotes,
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name)
 end
+puts "*" * 20
+puts "USERS"
+puts tp User.all
 
-
-3.times do
-
-  Event.create!(
-    start_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
-    duration: [30, 45,60,90,120].sample,
-    title: Faker::Food.dish,
-    description: Faker::Food.description,
-    price: Faker::Number.between(from: 1, to: 1000),
-    location: Faker::Address.street_address,
-    event_admin: User.all.sample
-    )
-
-
-end
-
+#create random events
 10.times do
 
-  Attendance.create!(
-    stripe_customer_id: Faker::Stripe.valid_card,
-    user: User.all.sample,
-    event: Event.all.sample
-  )
-
+  Event.create!(
+  start_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
+  duration: [15, 30, 45, 60, 90].sample,
+  title: ["Pikachu", "Bulbizar", "Salameche", "Carapuce", "Roucoul", "Ratata", "Chenipan"].sample,
+  description: "Mon métier est "+Faker::Job.title+"et mon film préféré est"+Faker::Movie.title,
+  price: Faker::Number.between(from: 1, to: 1000),
+  location: ["Mendoza", "Bariloche", "El Calafate", "Buenos Aires", "Salta", "Cordoba", "Rosario", "El Chalten"].sample,
+  event_admin: User.all.sample)
 end
+puts "*" * 20
+puts "EVENTS"
+puts tp Event.all
